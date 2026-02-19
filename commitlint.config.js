b7da215535118cbd302e5d5886c95ee04c57aa7f
Сальман Химишев <salman.khimishev01@gmail.com>
2026-02-19 20:31:09 +0300

    chore(config): добавлен конфиг commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'api',      // изменения в apps/api
+                'web',      // изменения в apps/web
+                'ui',       // изменения в packages/ui
+                'libs',     // изменения в других библиотеках
+                'config',   // настройки eslint, tsconfig и т.д.
+                'repo',     // настройки монорепо, turborepo, ci
+                'docs',     // документация
+            ],
+        ],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'chore',
+                'revert',
+                'ci',
+            ],
+        ],
+    },
+};
