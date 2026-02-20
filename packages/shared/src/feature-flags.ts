2ee695f0febcc7203fadb5a034795f30f7f4686b
Сальман Химишев <salman.khimishev01@gmail.com>
2026-02-20 20:59:14 +0300

    chore(repo): удалён packages/shared

--- packages/shared/src/feature-flags.ts
+++
@@ -1,5 +0,0 @@
-export function createFeatureFlagChecker<T extends Record<string, boolean>>(features: T) {
-	return function isFeatureEnabled(featureName: keyof T): boolean {
-		return features[featureName] ?? false
-	}
-}
