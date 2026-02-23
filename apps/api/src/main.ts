97235d480c8e4a8b99887f8d75779abd43ef87ea
Dmitrij Kolotusha <suvstreet@gmail.com>
2026-02-23 23:43:39 +0300

    feat(api): Добавить swagger.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors()
+
+	const config = new DocumentBuilder()
+		.setTitle('Tracker Task API')
+		.setDescription('API для управления проектами и задачами')
+		.setVersion('1.0.0')
+		.addBearerAuth()
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+
+	SwaggerModule.setup('api/docs', app, document)
+
 	await app.listen(4000)
 }
 
