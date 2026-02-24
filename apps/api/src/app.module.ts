450a0eb274d9b90033a3964270f627349ac3a4b5
Дмитрий <suvstreet@gmail.com>
2026-02-24 22:47:55 +0300

    feat(api): Добавить простую регистрацию пользователей. (#50)
    * feat: Добавить DTO для регистрации пользователя и удалить ссылки.
    
    * feat(api): добавить простую регистрацию пользователей

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
 
-import { LinksModule } from './links/links.module'
-
 import { AppService } from './app.service'
 import { AppController } from './app.controller'
-import { PrismaModule } from 'prisma/prisma.module'
+
+import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from '../prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-	imports: [LinksModule, PrismaModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
