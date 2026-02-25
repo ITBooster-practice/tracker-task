9b146671a57f9b5b274ff8758436982e8386a481
Fing0r <fantomma91@gmail.com>
2026-02-26 00:14:34 +0700

    feat(web): добавить AuthLayout

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import { FEATURES } from '@/hooks/use-feature-flag'
+import { notFound } from 'next/navigation'
+
+import { HStack } from '@repo/ui'
+
+interface Props {
+	children: React.ReactNode
+}
+
+export default function AuthLayout({ children }: Props) {
+	if (!FEATURES.AUTH) {
+		notFound()
+	}
+
+	return (
+		<HStack align='center' justify='center' className='min-h-screen bg-foreground'>
+			{children}
+		</HStack>
+	)
+}
