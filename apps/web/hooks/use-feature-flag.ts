c034a3c229ee5da1ec0b38971ede2a5171f7abdb
AlexMadM <alex.alexandrov.1988@gmail.com>
2026-03-03 19:32:57 -0800

    feat(web): add sprints dashboard page

--- apps/web/hooks/use-feature-flag.ts
+++ apps/web/hooks/use-feature-flag.ts
@@ -3,6 +3,7 @@ export const FEATURES = {
 	AUTH_DISABLE: process.env.NEXT_PUBLIC_DISABLE_AUTH === 'true',
 	PROJECTS: process.env.NEXT_PUBLIC_FEATURE_PROJECTS === 'true',
 	TASKS: process.env.NEXT_PUBLIC_FEATURE_TASKS === 'true',
+	SPRINTS: process.env.NEXT_PUBLIC_FEATURE_SPRINTS === 'true',
 } as const
 
 export const useFeatureFlag = (featureName: keyof typeof FEATURES) => {
