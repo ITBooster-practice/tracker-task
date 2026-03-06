2996337b5a59397fe991bd5cd69a52d140de6089
Dmitrij Kolotusha <suvstreet@gmail.com>
2026-03-06 19:14:38 +0300

    refactor(api): описать swagger документацию

--- apps/api/src/auth/dto/login.dto.ts
+++ apps/api/src/auth/dto/login.dto.ts
@@ -1,4 +1,11 @@
 import { createZodDto } from 'nestjs-zod'
 import { loginRequestSchema } from '@repo/types'
+import { ApiProperty } from '@nestjs/swagger'
 
-export class LoginRequestDto extends createZodDto(loginRequestSchema) {}
+export class LoginRequestDto extends createZodDto(loginRequestSchema) {
+	@ApiProperty({ example: 'user@example.com', description: 'Email пользователя' })
+	email: string
+
+	@ApiProperty({ example: '123456', description: 'Пароль' })
+	password: string
+}
--- apps/api/src/auth/dto/register.dto.ts
+++ apps/api/src/auth/dto/register.dto.ts
@@ -1,5 +1,15 @@
 import { createZodDto } from 'nestjs-zod'
 import { registerRequestSchema } from '@repo/types'
+import { ApiProperty } from '@nestjs/swagger'
 
 // Создаём DTO класс для NestJS и Swagger из схемы
-export class RegisterRequestDto extends createZodDto(registerRequestSchema) {}
+export class RegisterRequestDto extends createZodDto(registerRequestSchema) {
+	@ApiProperty({ example: 'Иван Иванов', description: 'Имя пользователя' })
+	name: string
+
+	@ApiProperty({ example: 'user@example.com', description: 'Email пользователя' })
+	email: string
+
+	@ApiProperty({ example: '123456', description: 'Пароль' })
+	password: string
+}
