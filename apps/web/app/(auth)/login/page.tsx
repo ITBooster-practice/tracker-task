ca352eaf972a9bdce077e6c6c57b1e164081cfe6
Fing0r <fantomma91@gmail.com>
2026-03-08 00:14:38 +0700

    feat(web): добавил использование api при логине

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { useLogin } from '@/hooks/api/use-auth'
+import { isApiError } from '@/lib/api/utils'
 import Link from 'next/link'
 
-import { Button, Form } from '@repo/ui'
+import { Button, Form, toast } from '@repo/ui'
 
 import { AuthFormLayout } from '../auth-form-layout'
 import { LoginForm } from './login-form'
@@ -11,8 +13,18 @@ import { LoginFormValues, useLoginForm } from './use-login-form'
 export default function LoginPage() {
 	const form = useLoginForm()
 
+	const loginMutation = useLogin()
+
 	const onSubmit = async (data: LoginFormValues) => {
-		console.log(data)
+		try {
+			await loginMutation.mutateAsync(data)
+		} catch (error) {
+			if (isApiError(error)) {
+				toast.error(error.message)
+			} else {
+				throw error
+			}
+		}
 	}
 
 	return (
