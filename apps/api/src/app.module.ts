ef9e7658d97a7a592b55f6727d0675ef685e2be0
Dmitrij Kolotusha <suvstreet@gmail.com>
2026-03-07 16:32:22 +0300

    feat(api): подключить Redis для кэширования данных

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,15 +9,20 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from '../prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
 import { CustomZodValidationPipe } from './common/providers/zod-validation.provider'
+import { RedisModule } from './common/redis/redis.module'
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		PrismaModule,
+		RedisModule,
+		AuthModule,
+	],
 	controllers: [AppController],
 	providers: [
 		AppService,
 		{ provide: APP_PIPE, useClass: CustomZodValidationPipe },
 		{ provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
-	]
+	],
 })
-
 export class AppModule {}
