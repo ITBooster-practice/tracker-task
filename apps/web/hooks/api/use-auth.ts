22d6f72186e5b738d6818877e94fa1c9c78c12a1
Fing0r <fantomma91@gmail.com>
2026-03-08 00:14:03 +0700

    feat(web): добавил use-auth для api

---
+++ apps/web/hooks/api/use-auth.ts
@@ -0,0 +1,37 @@
+import { authService } from '@/lib/api/auth-service'
+import { useMutation, useQuery } from '@tanstack/react-query'
+
+export const authKeys = {
+	register: ['register'],
+	login: ['login'],
+	logout: ['logout'],
+	getMe: ['me'],
+} as const
+
+export const useRegister = () => {
+	return useMutation({
+		mutationKey: authKeys.register,
+		mutationFn: authService.register,
+	})
+}
+
+export const useLogin = () => {
+	return useMutation({
+		mutationKey: authKeys.login,
+		mutationFn: authService.login,
+	})
+}
+
+export const useLogout = () => {
+	return useMutation({
+		mutationKey: authKeys.logout,
+		mutationFn: authService.logout,
+	})
+}
+
+export const useMe = () => {
+	return useQuery({
+		queryKey: authKeys.getMe,
+		queryFn: authService.getMe,
+	})
+}
