b60fa1eb65ba3f471288cea0c596e14a871200c8
Fing0r <fantomma91@gmail.com>
2026-03-11 01:15:21 +0700

    feat(web): добавить auth и protected route guard

--- apps/web/lib/session/index.ts
+++ apps/web/lib/session/index.ts
@@ -3,3 +3,5 @@ export { refreshSessionToken } from './session-refresh'
 export { getTokenExpiration, isTokenExpiredSoon } from './utils'
 
 export { SessionProvider } from './session-provider'
+export { AuthRouteGuard } from './auth-route-guard'
+export { ProtectedRouteGuard } from './protected-route-guard'
