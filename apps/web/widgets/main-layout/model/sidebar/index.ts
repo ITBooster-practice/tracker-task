f849359af9106b269b9d101096d2f70dbc0703c8
AlexMadM <alex.alexandrov.1988@gmail.com>
2026-03-10 19:04:42 -0700

    refactor(web): переработать структуру и логику Sidebar, обновить отображение секций и улучшить стили

--- apps/web/widgets/main-layout/model/sidebar/index.ts
+++ apps/web/widgets/main-layout/model/sidebar/index.ts
@@ -1,1 +1,14 @@
 export { useSideBarStore } from './store'
+export {
+	sidebarCurrentUser,
+	sidebarProjects,
+	sidebarSections,
+	sidebarWorkspace,
+} from './navigation'
+export type {
+	SidebarNavItem,
+	SidebarNavSection,
+	SidebarProjectItem,
+	SidebarUserCard,
+	SidebarWorkspace,
+} from './types'
