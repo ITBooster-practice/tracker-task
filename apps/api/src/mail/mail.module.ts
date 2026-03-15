aef8c7652b6ec1dc981514f16f80ac4f8c1e76bb
Сальман Химишев <salman.khimishev01@gmail.com>
2026-03-15 16:13:23 +0300

    chore(api): обновлён MailModule

--- apps/api/src/mail/mail.module.ts
+++ apps/api/src/mail/mail.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
 import { MailService } from './mail.service'
+import { MAIL_PROVIDER } from './mail.constants'
+import { BrevoMailProvider } from './providers/brevo-mail.provider'
 
 @Module({
-	providers: [MailService],
+	providers: [
+		MailService,
+		{
+			provide: MAIL_PROVIDER,
+			useClass: BrevoMailProvider,
+		},
+	],
 	exports: [MailService],
 })
 export class MailModule {}
