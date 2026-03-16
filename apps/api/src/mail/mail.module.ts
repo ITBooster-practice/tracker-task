3b95d81db943c48f72dbd1b06244988d260598b4
Сальман Химишев <salman.khimishev01@gmail.com>
2026-03-16 15:14:07 +0300

    feat(api): добавлен MailerModule в MailModule

--- apps/api/src/mail/mail.module.ts
+++ apps/api/src/mail/mail.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common'
 import { MailService } from './mail.service'
 import { MAIL_PROVIDER } from './mail.constants'
 import { BrevoMailProvider } from './providers/brevo-mail.provider'
+import { MailerModule } from '@nestjs-modules/mailer'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { getMailConfig } from './config/mail.config'
 
 @Module({
 	providers: [
@@ -10,6 +13,27 @@ import { BrevoMailProvider } from './providers/brevo-mail.provider'
 			provide: MAIL_PROVIDER,
 			useClass: BrevoMailProvider,
 		},
+	],
+	imports: [
+		MailerModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				const { host, port, secure, auth, from } = getMailConfig(configService)
+
+				return {
+					transport: {
+						host,
+						port,
+						secure,
+						auth,
+					},
+					defaults: {
+						from: `${from.name} <${from.address}>`,
+					},
+				}
+			},
+		}),
 	],
 	exports: [MailService],
 })
