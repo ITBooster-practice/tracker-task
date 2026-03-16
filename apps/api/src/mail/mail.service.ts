51ffc1d71bfc3dfbce8f7224ceb7b969908ecd72
Сальман Химишев <salman.khimishev01@gmail.com>
2026-03-16 08:04:28 +0300

    chore(api): добавлен метод sendWelcomeEmail в класс MailService

--- apps/api/src/mail/mail.service.ts
+++ apps/api/src/mail/mail.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+import type { MailProvider } from './mail.provider'
+import { MAIL_PROVIDER } from './mail.constants'
 
 @Injectable()
-export class MailService {}
+export class MailService {
+	constructor(
+		@Inject(MAIL_PROVIDER)
+		private readonly mailProvider: MailProvider,
+	) {}
+
+	async sendWelcomeEmail(email: string) {
+		await this.mailProvider.send({
+			to: email,
+			subject: 'Добро пожаловать',
+			text: 'Вы зарегистрированы',
+		})
+	}
+}
