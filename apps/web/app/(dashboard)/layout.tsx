e06c244ade26c9c85f5b02414d907f8903705dff
Fing0r <fantomma91@gmail.com>
2026-03-17 21:11:45 +0700

    feat(web): добавил prefetchQuery профиля в dashboard layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,15 +1,37 @@
+import { authKeys } from '@/hooks/api/use-auth'
+import { authService } from '@/lib/api/auth-service'
+import { isApiError } from '@/lib/api/utils'
+import { getQueryClient } from '@/lib/query-client'
+import { ROUTES } from '@/shared/config/routes'
 import { MainLayout } from '@/widgets/main-layout'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { redirect } from 'next/navigation'
 
 interface Props {
 	children: React.ReactNode
 	modal: React.ReactNode
 }
 
-export default function DashboardLayout({ children, modal }: Props) {
+export default async function DashboardLayout({ children, modal }: Props) {
+	const queryClient = getQueryClient()
+
+	try {
+		await queryClient.prefetchQuery({
+			queryKey: authKeys.getMe,
+			queryFn: authService.getMe,
+		})
+	} catch (error) {
+		if (isApiError(error) && error.statusCode === 401) {
+			redirect(ROUTES.login)
+		}
+
+		throw error
+	}
+
 	return (
-		<>
+		<HydrationBoundary state={dehydrate(queryClient)}>
 			<MainLayout>{children}</MainLayout>
 			{modal}
-		</>
+		</HydrationBoundary>
 	)
 }
