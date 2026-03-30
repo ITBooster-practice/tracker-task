2405990c0ba3bbbc32b7d0260ff62d12d53f0e95
Fing0r <fantomma91@gmail.com>
2026-03-30 23:40:38 +0700

    feat(ui): добавить use-media-query

---
+++ packages/ui/src/hooks/use-media-query.ts
@@ -0,0 +1,32 @@
+import { useCallback, useSyncExternalStore } from 'react'
+
+const getServerSnapshot = () => false
+
+/**
+ * @name useMediaQuery
+ * @description - Hook that manages a media query
+ * @category Browser
+ * @usage medium
+ *
+ * @browserapi window.matchMedia https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
+ *
+ * @example
+ * const matches = useMediaQuery('(max-width: 768px)');
+ */
+export const useMediaQuery = (query: string): boolean => {
+	const subscribe = useCallback(
+		(callback: () => void) => {
+			const matchMedia = window.matchMedia(query)
+
+			matchMedia.addEventListener('change', callback)
+			return () => {
+				matchMedia.removeEventListener('change', callback)
+			}
+		},
+		[query],
+	)
+
+	const getSnapshot = () => window.matchMedia(query).matches
+
+	return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
+}
