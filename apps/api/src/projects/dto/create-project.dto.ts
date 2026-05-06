a46d17c20fed0d062b6e09a20a84000a2b5af918
Сальман Химишев <salman.khimishev01@gmail.com>
2026-05-06 19:27:39 +0300

    feat(api): добавлен ProjectResponseDto

--- apps/api/src/projects/dto/create-project.dto.ts
+++ apps/api/src/projects/dto/create-project.dto.ts
@@ -2,16 +2,16 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { createProjectSchema } from '@repo/types'
 import { createZodDto } from 'nestjs-zod'
 
-export class createProjectDto extends createZodDto(createProjectSchema) {
+export class CreateProjectDto extends createZodDto(createProjectSchema) {
 	@ApiProperty({
 		example: 'Dream Project',
-		description: 'Название проекта (1–50 символов)',
+		description: 'Название проекта (1–100 символов)',
 	})
 	name: string
 
 	@ApiPropertyOptional({
 		example: 'My cool project',
-		description: 'Описание команды (до 500 символов)',
+		description: 'Описание проекта (до 500 символов)',
 	})
 	description?: string
 }
--- apps/api/src/projects/dto/update-project.dto.ts
+++ apps/api/src/projects/dto/update-project.dto.ts
@@ -2,16 +2,16 @@ import { ApiPropertyOptional } from '@nestjs/swagger'
 import { updateProjectSchema } from '@repo/types'
 import { createZodDto } from 'nestjs-zod'
 
-export class updateProjectDto extends createZodDto(updateProjectSchema) {
+export class UpdateProjectDto extends createZodDto(updateProjectSchema) {
 	@ApiPropertyOptional({
 		example: 'Dream Project',
-		description: 'Название проекта (1–50 символов)',
+		description: 'Название проекта (1–100 символов)',
 	})
 	name?: string
 
 	@ApiPropertyOptional({
 		example: 'My cool project',
-		description: 'Описание команды (до 500 символов)',
+		description: 'Описание проекта (до 500 символов)',
 	})
 	description?: string
 }
