33d773282075ad09030e73139f2d9e057b984a7f
Dmitry <suvstreet@gmail.com>
2026-05-23 19:05:56 +0300

    docs: добавить заметку для релиза v1.0.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,35 +1,35 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-enum': [
-            2,
-            'always',
-            [
-                'api',      // изменения в apps/api
-                'web',      // изменения в apps/web
-                'ui',       // изменения в packages/ui
-				'pack',     // изменения в packages
-                'libs',     // изменения в других библиотеках
-                'config',   // настройки eslint, tsconfig и т.д.
-                'repo',     // настройки монорепо, turborepo, ci
-                'docs',     // документация
-            ],
-        ],
-        'type-enum': [
-            2,
-            'always',
-            [
-                'feat',
-                'fix',
-                'docs',
-                'style',
-                'refactor',
-                'perf',
-                'test',
-                'chore',
-                'revert',
-                'ci',
-            ],
-        ],
-    },
-};
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'scope-enum': [
+			2,
+			'always',
+			[
+				'api', // изменения в apps/api
+				'web', // изменения в apps/web
+				'ui', // изменения в packages/ui
+				'pack', // изменения в packages
+				'libs', // изменения в других библиотеках
+				'config', // настройки eslint, tsconfig и т.д.
+				'repo', // настройки монорепо, turborepo, ci
+				'docs', // документация
+			],
+		],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'docs',
+				'style',
+				'refactor',
+				'perf',
+				'test',
+				'chore',
+				'revert',
+				'ci',
+			],
+		],
+	},
+}
